6d4122b40ad19e976e2fd6258458f0ec7257fdde
aml_gamal <amlgamal@std.mans.edu.eg>
2024-08-16 12:40:29 +0300

    amll

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
         'chat': (context) => chat(),
       },
       debugShowCheckedModeBanner: false,
-      initialRoute: 'chat',
+      initialRoute: 'login',
     );
   }
 }
